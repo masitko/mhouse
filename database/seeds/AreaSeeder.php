940b964f6fefad476ed941b9382ed09d01c12dae
vagrant <vagrant@homestead>
2019-02-20 22:17:18 +0000

    added munthams favourite colours

--- database/seeds/AreaSeeder.php
+++ database/seeds/AreaSeeder.php
@@ -12,8 +12,8 @@ class AreaSeeder extends Seeder
         [ 'name' => 'Money management', 'description' => 'Description for Money management', 'order' => 30, 'colour' => '#B98EA7'],
         [ 'name' => 'Accessing the community', 'description' => 'Description for Accessing the community', 'order' => 40, 'colour' => '#77B28C'],
         [ 'name' => 'Work ready/employability', 'description' => 'Description for Work ready/employability', 'order' => 50, 'colour' => '#CBE896'],
-        [ 'name' => 'Health and well-being', 'description' => 'Description for Health and well-being', 'order' => 60, 'colour' => '#FFC09F'],
-        [ 'name' => 'Learning and development', 'description' => 'Description for Learning and development', 'order' => 70, 'colour' => '#58A4B0'],
+        [ 'name' => 'Health and well-being', 'description' => 'Description for Health and well-being', 'order' => 60, 'colour' => '#8D89A6'],
+        [ 'name' => 'Learning and development', 'description' => 'Description for Learning and development', 'order' => 70, 'colour' => '#2F60BC'],
     ];
 
     public function run()
