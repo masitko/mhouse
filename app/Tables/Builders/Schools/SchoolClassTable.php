669dbb27af93bf00d83ca3bc3d4b22f52b3084b7
vagrant <vagrant@homestead>
2019-03-29 12:09:45 +0000

    fixed another logged out error

--- app/Tables/Builders/Schools/SchoolClassTable.php
+++ app/Tables/Builders/Schools/SchoolClassTable.php
@@ -6,14 +6,17 @@ use App\SchoolClass;
 use App\Traits\CurrentUser;
 use LaravelEnso\VueDatatable\app\Classes\Table;
 
-class SchoolClassTable extends Table {
-  
+class SchoolClassTable extends Table
+{
+
   use CurrentUser;
 
   protected $templatePath = __DIR__ . '/../../Templates/Schools/schoolClasses.json';
 
-  public function query() {
-    return SchoolClass::select(\DB::raw('
+  public function query()
+  {
+    if ($this->getCurrentUser())
+      return SchoolClass::select(\DB::raw('
           school_classes.*, school_classes.id as "dtRowId"
         '))->where('school_id', $this->getCurrentUser()->school_id);
   }
