5c69879321d5020df322f2e2b350881ba380e798
vagrant <vagrant@homestead>
2019-06-11 10:58:25 +0000

    updated ethnicities

--- app/Enums/Ethnicity.php
+++ app/Enums/Ethnicity.php
@@ -6,11 +6,29 @@ use LaravelEnso\Helpers\app\Classes\Enum;
 
 class Ethnicity extends Enum
 {
-    const ABAN = 1;
-    const AIND = 2;
-    const AOTH = 3;
-    const APKN = 4;
-    const BAFR = 5;
+    const ABAN = 10;
+    const AIND = 20;
+    const AOTH = 30;
+    const APKN = 40;
+    const BAFR = 50;
+ 
+    const BCRB = 60;
+    const BOTH = 70;
+    const CHNE = 80;
+    const MOTH = 90;
+    const MWAS = 100;
+ 
+    const MWBA = 110;
+    const MWBC = 120;
+    const NOBT = 130;
+    const OOTH = 140;
+    const REFU = 150;
+
+    const WBRI = 160;
+    const WIRI = 170;
+    const WIRT = 180;
+    const WOTH = 190;
+    const WROM = 200;
 
     protected static $data = [
         self::ABAN => 'ABAN',
@@ -18,5 +36,23 @@ class Ethnicity extends Enum
         self::AOTH => 'AOTH',
         self::APKN => 'APKN',
         self::BAFR => 'BAFR',
+ 
+        self::BCRB => 'BCRB',
+        self::BOTH => 'BOTH',
+        self::CHNE => 'CHNE',
+        self::MOTH => 'MOTH',
+        self::MWAS => 'MWAS',
+
+        self::MWBA => 'MWBA',
+        self::MWBC => 'MWBC',
+        self::NOBT => 'NOBT',
+        self::OOTH => 'OOTH',
+        self::REFU => 'REFU',
+
+        self::WBRI => 'WBRI',
+        self::WIRI => 'WIRI',
+        self::WIRT => 'WIRT',
+        self::WOTH => 'WOTH',
+        self::WROM => 'WROM',
     ];
 }
