307ba70cd8054b527da8ecbca8beb714c061b77e
vagrant <vagrant@homestead>
2019-06-11 22:42:18 +0000

    added filter to users and students tables

--- packages/laravel-enso/core/src/app/Tables/Builders/UserTable.php
+++ packages/laravel-enso/core/src/app/Tables/Builders/UserTable.php
@@ -12,12 +12,14 @@ class UserTable extends Table {
   private const TemplatePath = __DIR__ . '/../Templates/';
   protected $templatePath;
 
+  protected $type;
 
   public function __construct( array $request = [] )
   {
     // getting template corresponding to the route name (user, student, ...)
     $segments = explode( '.', Route::currentRouteName() );
-    $this->templatePath = self::TemplatePath . $segments[1] . '.json';
+    $this->type = $segments[1];
+    $this->templatePath = self::TemplatePath . $this->type . '.json';
     parent::__construct($request);
   }
 
@@ -31,6 +33,7 @@ class UserTable extends Table {
     ))
     // ->join('people', 'users.person_id', '=', 'people.id')
       ->join('roles', 'users.role_id', '=', 'roles.id')
+      ->where( 'roles.name', $this->type==='students'?'=':'<>', 'student')
       ->leftJoin('schools', 'users.school_id', '=', 'schools.id')
       ->leftJoin('avatars', 'users.id', '=', 'avatars.user_id');
   }
