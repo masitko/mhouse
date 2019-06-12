3bfe16e89522b2f694b2eaf99d7c57dd8c495fba
vagrant <vagrant@homestead>
2019-06-12 08:24:40 +0000

    fixes to student form

--- resources/js/pages/administration/users/Create.vue
+++ resources/js/pages/administration/users/Create.vue
@@ -2,21 +2,21 @@
 <div class="columns is-centered">
         <div class="column is-three-quarters-desktop is-full-touch">
             <enso-form class="box has-background-light raises-on-hover animated fadeIn">
-                <template slot="group_id"
+                <!-- <template slot="group_id"
                     slot-scope="{ field, errors, i18n }">
                     <select-field :errors="errors"
                         :field="field"
                         :i18n="i18n"
                         @input="pivotParams.userGroups.id = $event"/>
-                </template>
-                <!-- <template slot="role_id"
+                </template> -->
+                <template slot="role_id"
                     slot-scope="{ field, errors, i18n }">
                     <select-field :errors="errors"
                         :field="field"
                         :i18n="i18n"
                         :pivot-params="pivotParams"
                         />
-                </template> -->
+                </template>
                 <template slot="password"
                     slot-scope="{ field, errors, i18n }">
                     <input-field :errors="errors"
@@ -54,7 +54,10 @@ export default {
     components: { EnsoForm, InputField, SelectField, PasswordStrength },
 
     data: () => ({
-        pivotParams: { userGroups: { id: null } },
+        pivotParams: { 
+          roles: { name: 'student'},
+          userGroups: { id: null } 
+          },
         password: null,
         passwordConfirmation: null,
 
