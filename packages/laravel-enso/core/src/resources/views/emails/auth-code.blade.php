9f2609bc03b245c636c57901cc488aeaa9c64ca9
vagrant <vagrant@homestead>
2019-06-12 09:08:15 +0000

    fixed auth code email wording

--- packages/laravel-enso/core/src/resources/views/emails/auth-code.blade.php
+++ packages/laravel-enso/core/src/resources/views/emails/auth-code.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 {{ __('Hi :name', ['name' => $name]) }},
 
-{{ __("We've noticed you are logging-in from a new place.") }}
+{{ __("We've noticed you are logging-in from a new location.") }}
 {{ __('Please use the following code to login to the system:', ['code' => $code]) }} 
 <strong>{{$code}}</strong>
 
